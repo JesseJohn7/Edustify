3d9a45a0d2e6a6fab3992f71969e4b2a5b2bfac6
JesseJohn7 <jessejohn260@gmail.com>
2025-04-24 02:05:43 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/navbar'
 import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Program'
 
 const App = () => {
   return (
     <div>
      <Navbar />
      <Hero />
+     <Programs />
     </div>
   )
 }
