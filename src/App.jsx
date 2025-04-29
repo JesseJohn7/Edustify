594386c5db38870bc644786dc4685b285df3ea3f
JesseJohn7 <jessejohn260@gmail.com>
2025-04-30 00:02:07 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Programs from './components/Programs/Program'
 import Title from './components/Title/Title'
 import About from './components/About/About'
 import Campus from './components/Campus/Campus'
+import Testimonials from './components/Testimonials/Testimonial'
 
 const App = () => {
   return (
@@ -17,6 +18,8 @@ const App = () => {
      <About />
      <Title  subTitle = "Gallery" title ="Campus Photos"/>
      <Campus />
+     <Title  subTitle = "Testimonials" title ="What Student's Say"/>
+     <Testimonials  />
      </div>
      
     </div>
