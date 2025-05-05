1d4b6d3bede58716a05a92388acc3097de990c0e
JesseJohn7 <jessejohn260@gmail.com>
2025-05-05 13:42:17 +0100

    initial commit

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -7,13 +7,72 @@ import user_2 from '../../assets/user-2.png'
 import user_3 from '../../assets/user-3.png'
 import user_4 from '../../assets/user-4.png'
 const Testimonial = () => {
+    const slideForward = () =>{
+
+    }
+
+    const slideBackward =() =>{
+        
+    }
   return (
     <div className='testimonial'>
-        <img src={next_icon} alt="next_icon" className='next-btn' />
-        <img src={back_icon} alt="back-icon" className='back-btn' />
+        <img src={next_icon} alt="next_icon" className='next-btn'  onClick={slideForward}/>
+        <img src={back_icon} alt="back-icon" className='back-btn'  onClick={slideBackward}/>
 
         <div className="slider">
+            <ul>
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_1} alt="" />
+                            <div>
+                                <h3>Kevin Williams</h3>
+                                <span>Edustify , USA</span>
+                            </div>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quos sit iusto quae, similique dolorem rem doloribus qui, accusamus id reprehenderit excepturi! Illum ipsa quos fugit aspernatur, quisquam aliquam eligendi.</p>
+                    </div>
+                </li>
 
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_2} alt="" />
+                            <div>
+                                <h3>Kevin Williams</h3>
+                                <span>Edustify , USA</span>
+                            </div>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quos sit iusto quae, similique dolorem rem doloribus qui, accusamus id reprehenderit excepturi! Illum ipsa quos fugit aspernatur, quisquam aliquam eligendi.</p>
+                    </div>
+                </li>
+
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_3} alt="" />
+                            <div>
+                                <h3>Kevin Williams</h3>
+                                <span>Edustify , USA</span>
+                            </div>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quos sit iusto quae, similique dolorem rem doloribus qui, accusamus id reprehenderit excepturi! Illum ipsa quos fugit aspernatur, quisquam aliquam eligendi.</p>
+                    </div>
+                </li>
+
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_4} alt="" />
+                            <div>
+                                <h3>Kevin Williams</h3>
+                                <span>Edustify , USA</span>
+                            </div>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quos sit iusto quae, similique dolorem rem doloribus qui, accusamus id reprehenderit excepturi! Illum ipsa quos fugit aspernatur, quisquam aliquam eligendi.</p>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
   )
